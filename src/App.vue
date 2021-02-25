027e97a7ffcc407e08f619ea9eeb83d4ea46059d
rabisse <rabisse@gmail.com>
2021-02-25 11:09:36 +0000

    data shows up in vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    
+    <h1>UK National Energy Usage Sources</h1>
+    <div>
+
+    </div>
   </div>
 </template>
 
@@ -9,7 +12,24 @@
 
 export default {
   name: 'App',
-  
+  data(){
+    return {
+      rawEnergyData: [],
+      translatedEnergyData: [],
+      // startTime: null,
+      // endTime: null,
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(res => res.json())
+    .then(data => this.rawEnergyData = data.data.generationmix)
+    // .then(data => this.startTime = data.data.from)
+    // .then(data => this.endTime = data.data.to)
+  }
 }
 </script>
 
